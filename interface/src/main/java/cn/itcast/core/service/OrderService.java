899037cb559574f2c97f29ee0712e19c100e24b9
King_slz <1453133181@qq.com>
2019-01-10 16:32:04 +0800

    孙 一点小的修改

--- interface/src/main/java/cn/itcast/core/service/OrderService.java
+++ interface/src/main/java/cn/itcast/core/service/OrderService.java
@@ -1,9 +1,14 @@
 package cn.itcast.core.service;
 
+import cn.itcast.core.pojo.entity.OrdersAnalye;
+import cn.itcast.core.pojo.entity.PageResult;
 import cn.itcast.core.pojo.entity.Result;
 import cn.itcast.core.pojo.log.PayLog;
 import cn.itcast.core.pojo.order.Order;
+import cn.itcast.core.pojo.order.OrderItem;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public interface OrderService {
 
@@ -12,4 +17,8 @@ public interface OrderService {
     public PayLog getPayLogByUserName(String userName);
 
     public void updatePayStatus(String userName);
+
+    PageResult search(OrderItem orderItem, Integer page, Integer rows);
+
+    List<OrdersAnalye> analye();
 }
--- pojo/src/main/java/cn/itcast/core/pojo/order/Order.java
+++ pojo/src/main/java/cn/itcast/core/pojo/order/Order.java
@@ -3,11 +3,23 @@ package cn.itcast.core.pojo.order;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 public class Order implements Serializable {
+    private List<OrderItem> orderItemList;
+
+    public List <OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(List <OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+
     /**
      * 订单id
      */
+
     private Long orderId;
 
     /**
