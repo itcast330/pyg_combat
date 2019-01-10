ebd1cf5cd7342b35b472a962aeff962ce444001c
King_slz <1453133181@qq.com>
2019-01-10 17:23:40 +0800

    孙       一点小的修改2

--- interface/src/main/java/cn/itcast/core/service/OrderService.java
+++ interface/src/main/java/cn/itcast/core/service/OrderService.java
@@ -22,3 +22,4 @@ public interface OrderService {
 
     List<OrdersAnalye> analye();
 }
+
--- solr_util/src/main/java/cn/itcast/core/util/BrandController123.java
+++ solr_util/src/main/java/cn/itcast/core/util/BrandController123.java
@@ -6,6 +6,7 @@ public class BrandController123 {
         System.out.println("12222333333333333");
         System.out.println("12222244444444444");
         System.out.println("12222222222222222");
+        System.out.println ("kkkk" );
     }
 
 
